a09b252563d639621b26ca9326affe9d957cf9a9
Hamza-Burhan <hamzaburhan0985@gmail.com>
2024-05-10 15:50:29 +0500

    getDerived run before setState so it will take the props value not setstate value

--- src/main/components/child.jsx
+++ src/main/components/child.jsx
@@ -10,10 +10,10 @@ import React from 'react'
   static getDerivedStateFromProps(props,state){
     return {model: props.carModel}
   }
-  componentDidMount() {
-      this.setState({ model: 'dala' })
-      console.log('this.state.model in setState callback: componentDidMount', this.state.model);
-    
+ 
+  handleClick = () => {
+    this.setState({ model: 'changedByButton'})
+    console.log('this.state.model: ', this.state.model);
   }
   render(){
     return (
@@ -21,9 +21,7 @@ import React from 'react'
           <h2>First Child </h2>
           <h2>props from parent {this.props.color}</h2>
           <h2>state changed by props and after mounting {this.state.model}</h2>
-
-
-
+          <button onClick={this.handleClick}>Click</button>
       </>
     )
   }
