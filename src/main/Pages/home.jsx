e23d477e05d7199a7f431b7cec71f6660afe8bd8
Hamza-Burhan <hamzaburhan0985@gmail.com>
2024-05-15 10:31:15 +0500

    mutationns

--- src/main/Pages/home.jsx
+++ src/main/Pages/home.jsx
@@ -5,27 +5,48 @@ import React from 'react'
 import Childkachild from '../components/childkachild';
 import ErrorBoundry from '../ErrorBoundry';
 
-import {gql,useQuery } from '@apollo/client'
-const GET_TODOS = gql`
-query {
-  post(id: 1) {
-    id
-    title
-    body
+import {gql,useMutation } from '@apollo/client'
+const POST_MUTATION = gql`
+  mutation(
+    $input: CreatePostInput!
+  ){
+    createPost(input: $input){
+      id
+      title
+      body
+    }
   }
-}
-`;
+`
 
 
 export default function Home() {
-  const {loading,error, data} = useQuery(GET_TODOS)
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+  const [title, setTitle] = useState('')
+  const [body, setbody] = useState('')
+  const [createPost, {error}] = useMutation(POST_MUTATION)
 
-  console.log("data", data)
+  const handleClick = () => {
+    const input = {
+      input:{
+        title: title,
+        body: body,
+      }
+     
+    }
+    const createdPost = createPost({
+      variables:input
+    })
+    
+    console.log('input: ', createdPost);
+
+  }
     return (
       <>
-        <h2>Home page</h2>
+        <h2>Home page Mutation exap</h2>
+        <label>title</label>
+        <input type="text" name="title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+        <label>Body</label>
+        <input type="text" name="body" value={body} onChange={(e) => setbody(e.target.value)}/>
+        <button onClick={handleClick}>click</button>
       </>
     )
  
